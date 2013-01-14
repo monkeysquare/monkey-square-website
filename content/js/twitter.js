36da13892f00b3aa710bba743fe04c09dfdacba5
Dale Ragan <dale@ragan.io>
2013-01-14 01:54:47 -0500

    Fixed twitter feed errors.

--- content/js/twitter.js
+++ content/js/twitter.js
@@ -62,17 +62,16 @@ function showTwitterFeed(tweets, twitter_user) {
       content = '';
 
   for (var t in tweets) {
-    content += '<li>'+'<p>'+'<a href="http://twitter.com/'+twitter_user+'/status/'+tweets[t].id_str+'">'+prettyDate(tweets[t].created_at)+'</a>'+linkifyTweet(tweets[t].text.replace(/\n/g, '<br>'), tweets[t].entities.urls)+'</p>'+'</li>';
+    content += '<li>'+'<a href="http://twitter.com/'+twitter_user+'/status/'+tweets[t].id_str+'">'+prettyDate(tweets[t].created_at)+'</a>  '+linkifyTweet(tweets[t].text.replace(/\n/g, '<br>'), tweets[t].entities.urls)+'</li>';
   }
   timeline.innerHTML = content;
 }
 
 function getTwitterFeed(user, count, replies) {
   count = parseInt(count, 10);
-  $.ajax({
-      url: "http://api.twitter.com/1/statuses/user_timeline/" + user + ".json?trim_user=true&count=" + (count + 20) + "&include_entities=1&exclude_replies=" + (replies ? "0" : "1") + "&callback=?"
-    , type: 'jsonp'
-    , error: function (err) { $('#tweets li.loading').addClass('error').text("Twitter's busted"); }
-    , success: function(data) { showTwitterFeed(data.slice(0, count), user); }
-  })
+  $.getJSON('https://api.twitter.com/1/statuses/user_timeline/' + user + '.json?trim_user=true&count=' + (count + 20) + '&include_entities=1&exclude_replies=' + (replies ? "0" : "1") + '&callback=?', function(data){
+    showTwitterFeed(data.slice(0, count), user);
+  });
 }
+
+function myFunction(r) { console.log(r); }
